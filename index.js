ffd596b4a2776ccc8b024e3400e4c9dbda7f6c23
Ben Spector <30628696+benspector3@users.noreply.github.com>
2023-10-05 19:18:34 +0000

    adding subtract function and tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log("hello world!");
 console.log("What is up doc?");
+const subtract = (a, b) => a - b; 
+
+const result = subtract(5, 3)
+console.log(result);
+console.log(subtract(5, 3));
+
+const name = 'ben';
+console.log(name);
+console.log('ben');
